dfd2006e10d8c606980824d8ea5880713c3fa906
Nick R. Katsipoulakis <nick.katsip@gmail.com>
2017-07-26 17:12:31 -0400

    added export functionality for exporting information from embedded HSQLDB and updated the API to offer selection based on ExtendedAttribute names

--- src/main/java/edu/pitt/cs/admt/cytoscape/annotations/db/NetworkStorageUtility.java
+++ src/main/java/edu/pitt/cs/admt/cytoscape/annotations/db/NetworkStorageUtility.java
@@ -5,6 +5,7 @@ import edu.pitt.cs.admt.cytoscape.annotations.db.entity.*;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Collection;
+import java.util.Optional;
 
 /**
  * @author Nikos R. Katsipoulakis
@@ -17,6 +18,8 @@ public class NetworkStorageUtility {
                                       Collection<AnnotToEntity> annotationToNode,
                                       Collection<AnnotToEntity> annotationToEdge)
       throws SQLException, IOException {
+    if (storageDelegate == null)
+      return;
     storageDelegate.insertNodes(nodes);
     storageDelegate.insertEdges(edges);
     storageDelegate.insertAnnotations(annotations);
@@ -27,5 +30,61 @@ public class NetworkStorageUtility {
     for (AnnotToEntity e : annotationToEdge)
       storageDelegate.attachAnnotationToEdge(e.getAnnotationId(), e.getEntityId(),
           e.getExtendedAttributeId(), e.getValue());
+  }
+
+  public static Optional<Collection<Node>> exportNodes(StorageDelegate delegate)
+      throws SQLException {
+    if (delegate != null)
+      return delegate.getNodes();
+    return Optional.empty();
+  }
+
+  public static Optional<Collection<Edge>> exportEdges(StorageDelegate delegate)
+      throws SQLException {
+    if (delegate != null)
+      return delegate.getEdges();
+    return Optional.empty();
+  }
+
+  public static Optional<Collection<Annotation>> exportAnnotations(StorageDelegate delegate)
+      throws SQLException {
+    if (delegate != null)
+      return delegate.getAnnotations();
+    return Optional.empty();
+  }
+
+  public static Optional<Collection<ExtendedAttribute>> exportExtendedAttributes(
+      StorageDelegate delegate) throws SQLException {
+    if (delegate != null)
+      return delegate.getExtendedAttributes();
+    return Optional.empty();
+  }
+
+  public static Optional<Collection<AnnotToEntity>> exportAnnotationToNodes(
+      StorageDelegate delegate) throws SQLException, IOException, ClassNotFoundException {
+    if (delegate != null)
+      return delegate.getAnnotationsToNodes();
+    return Optional.empty();
+  }
+
+  public static Optional<Collection<AnnotToEntity>> exportAnnotationToEdges(
+      StorageDelegate delegate) throws SQLException, IOException, ClassNotFoundException {
+    if (delegate != null)
+      return delegate.getAnnotationsToEdges();
+    return Optional.empty();
+  }
+
+  public static Optional<Collection<Node>> selectNodesWithExtendedAttributeName(
+      StorageDelegate delegate, String name) throws SQLException {
+    if (delegate != null)
+      return delegate.selectNodesWithExtendedAttribute(name);
+    return Optional.empty();
+  }
+
+  public static Optional<Collection<Edge>> selectEdgesWithExtendedAttribute(
+      StorageDelegate delegate, String name) throws SQLException {
+    if (delegate != null)
+      return delegate.selectEdgesWithExtendedAttribute(name);
+    return Optional.empty();
   }
 }
--- src/main/java/edu/pitt/cs/admt/cytoscape/annotations/db/StorageDelegate.java
+++ src/main/java/edu/pitt/cs/admt/cytoscape/annotations/db/StorageDelegate.java
@@ -7,7 +7,9 @@ import org.hsqldb.jdbc.JDBCDriver;
 
 import java.io.*;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
 import java.util.Properties;
 
 /**
@@ -111,6 +113,16 @@ class StorageDelegate {
     connection.commit();
   }
 
+  Optional<Collection<Node>> getNodes() throws SQLException {
+    PreparedStatement statement = connection.prepareStatement("SELECT * FROM " +
+        AnnotationSchema.NODE_TABLE);
+    Collection<Node> nodes = new ArrayList<>();
+    ResultSet resultSet = statement.executeQuery();
+    while (resultSet.next())
+      nodes.add(new Node(resultSet.getInt(1)));
+    return nodes.size() > 0 ? Optional.of(nodes) : Optional.empty();
+  }
+
   void insertNewEdge(int edgeId, int source, int destination) throws SQLException {
     connection.setAutoCommit(false);
     PreparedStatement statement = connection.prepareStatement(AnnotationSchema.INSERT_EDGE);
@@ -135,6 +147,16 @@ class StorageDelegate {
     connection.commit();
   }
 
+  Optional<Collection<Edge>> getEdges() throws SQLException {
+    PreparedStatement statement = connection.prepareStatement("SELECT * FROM " +
+        AnnotationSchema.EDGE_TABLE);
+    Collection<Edge> edges = new ArrayList<>();
+    ResultSet resultSet = statement.executeQuery();
+    while (resultSet.next())
+      edges.add(new Edge(resultSet.getInt(1), resultSet.getInt(2), resultSet.getInt(3)));
+    return edges.size() > 0 ? Optional.of(edges) : Optional.empty();
+  }
+
   void insertAnnotation(int annotationId, String description) throws IllegalArgumentException,
       SQLException {
     connection.setAutoCommit(false);
@@ -157,6 +179,16 @@ class StorageDelegate {
     }
     statement.executeBatch();
     connection.commit();
+  }
+
+  Optional<Collection<Annotation>> getAnnotations() throws SQLException {
+    PreparedStatement statement = connection.prepareStatement("SELECT * FROM " +
+        AnnotationSchema.ANNOTATION_TABLE);
+    ResultSet resultSet = statement.executeQuery();
+    Collection<Annotation> annotations = new ArrayList<>();
+    while (resultSet.next())
+      annotations.add(new Annotation(resultSet.getInt(1), resultSet.getString(2)));
+    return annotations.size() > 0 ? Optional.of(annotations) : Optional.empty();
   }
 
   void attachAnnotationToNode(int annotationId, int nodeId, Integer extendedAttributeId,
@@ -209,6 +241,38 @@ class StorageDelegate {
     connection.commit();
   }
 
+  Optional<Collection<AnnotToEntity>> getAnnotationsToNodes() throws SQLException, IOException,
+      ClassNotFoundException {
+    String sqlStatement = "SELECT * FROM " + AnnotationSchema.ANNOT_TO_NODE_TABLE;
+    return getAnnotationToEntities(sqlStatement);
+  }
+
+  Optional<Collection<AnnotToEntity>> getAnnotationsToEdges() throws SQLException, IOException,
+      ClassNotFoundException {
+    String sqlStatement = "SELECT * FROM " + AnnotationSchema.ANNOT_TO_EDGE_TABLE;
+    return getAnnotationToEntities(sqlStatement);
+  }
+
+  private Optional<Collection<AnnotToEntity>> getAnnotationToEntities(String sqlStatement)
+      throws SQLException, IOException, ClassNotFoundException {
+    PreparedStatement statement = connection.prepareStatement(sqlStatement);
+    ResultSet resultSet = statement.executeQuery();
+    Collection<AnnotToEntity> annotToEntities = new ArrayList<>();
+    while (resultSet.next()) {
+      int annotationId = resultSet.getInt(1);
+      int nodeId = resultSet.getInt(2);
+      Integer extendedAttributeId = resultSet.getInt(3);
+      byte[] binaryValue = resultSet.getBytes(4);
+      if (!resultSet.wasNull()) {
+        annotToEntities.add(new AnnotToEntity(annotationId, nodeId, extendedAttributeId,
+            convertToObject(binaryValue)));
+      } else {
+        annotToEntities.add(new AnnotToEntity(annotationId, nodeId, null, null));
+      }
+    }
+    return annotToEntities.size() > 0 ? Optional.of(annotToEntities) : Optional.empty();
+  }
+
   void insertAnnotationExtendedAttribute(int extendedAttributeId, String name,
                                          ExtendedAttributeType type)
       throws IllegalArgumentException, SQLException {
@@ -240,8 +304,31 @@ class StorageDelegate {
     statement.executeBatch();
     connection.commit();
   }
+
+  Optional<Collection<ExtendedAttribute>> getExtendedAttributes() throws SQLException {
+    PreparedStatement statement = connection.prepareStatement("SELECT * FROM " +
+        AnnotationSchema.ANNOT_EXT_ATTR_TABLE);
+    ResultSet resultSet = statement.executeQuery();
+    Collection<ExtendedAttribute> attributes = new ArrayList<>();
+    while (resultSet.next()) {
+      int id = resultSet.getInt(1);
+      String name = resultSet.getString(2);
+      String typeName = resultSet.getString(3);
+      if (ExtendedAttributeType.CHAR.equalsName(typeName))
+        attributes.add(new ExtendedAttribute(id, name, ExtendedAttributeType.CHAR));
+      else if (ExtendedAttributeType.BOOLEAN.equalsName(typeName))
+        attributes.add(new ExtendedAttribute(id, name, ExtendedAttributeType.BOOLEAN));
+      else if (ExtendedAttributeType.FLOAT.equalsName(typeName))
+        attributes.add(new ExtendedAttribute(id, name, ExtendedAttributeType.FLOAT));
+      else if (ExtendedAttributeType.INT.equalsName(typeName))
+        attributes.add(new ExtendedAttribute(id, name, ExtendedAttributeType.INT));
+      else if (ExtendedAttributeType.STRING.equalsName(typeName))
+        attributes.add(new ExtendedAttribute(id, name, ExtendedAttributeType.STRING));
+    }
+    return attributes.size() > 0 ? Optional.of(attributes) : Optional.empty();
+  }
   
-  private static Object converToObject(byte[] binaryObject)
+  private static Object convertToObject(byte[] binaryObject)
       throws IOException, ClassNotFoundException {
     try (ByteArrayInputStream byteStream = new ByteArrayInputStream(binaryObject)) {
       try (ObjectInputStream objectStream = new ObjectInputStream(byteStream)) {
@@ -257,5 +344,40 @@ class StorageDelegate {
         return binaryStream.toByteArray();
       }
     }
+  }
+
+  Optional<Collection<Node>> selectNodesWithExtendedAttribute(String name) throws SQLException {
+    String query = "SELECT DISTINCT suid FROM " + AnnotationSchema.ANNOT_TO_NODE_TABLE +
+        " JOIN " + AnnotationSchema.ANNOT_EXT_ATTR_TABLE + " ON " +
+        AnnotationSchema.ANNOT_TO_NODE_TABLE + ".ext_attr_id = " +
+        AnnotationSchema.ANNOT_EXT_ATTR_TABLE + ".id WHERE " +
+        AnnotationSchema.ANNOT_EXT_ATTR_TABLE + ".name = ?";
+    if (name == null || name.length() == 0)
+      return Optional.empty();
+    PreparedStatement statement = connection.prepareStatement(query);
+    statement.setString(1, name);
+    ResultSet resultSet = statement.executeQuery();
+    Collection<Node> nodes = new ArrayList<>();
+    while (resultSet.next())
+      nodes.add(new Node(resultSet.getInt(1)));
+    return nodes.size() > 0 ? Optional.of(nodes) : Optional.empty();
+  }
+
+  Optional<Collection<Edge>> selectEdgesWithExtendedAttribute(String name) throws SQLException {
+    String query = "SELECT DISTINCT e.suid, e.source, e.destination FROM " +
+        AnnotationSchema.EDGE_TABLE + " as e, " +
+        AnnotationSchema.ANNOT_TO_EDGE_TABLE + " as a_e, " +
+        AnnotationSchema.ANNOT_EXT_ATTR_TABLE + " as a_ext_attr " +
+        "WHERE e.suid = a_e.suid AND a_ext_attr.id = a_e.ext_attr_id AND a_ext_attr" +
+        ".name = ?";
+    if (name == null || name.length() == 0)
+      return Optional.empty();
+    PreparedStatement statement = connection.prepareStatement(query);
+    statement.setString(1, name);
+    ResultSet resultSet = statement.executeQuery();
+    Collection<Edge> edges = new ArrayList<>();
+    while (resultSet.next())
+      edges.add(new Edge(resultSet.getInt(1), resultSet.getInt(2), resultSet.getInt(3)));
+    return edges.size() > 0 ? Optional.of(edges) : Optional.empty();
   }
 }
