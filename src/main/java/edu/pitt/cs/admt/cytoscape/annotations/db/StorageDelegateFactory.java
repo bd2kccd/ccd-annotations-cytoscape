3cba5b234cc096e1fa4573059279c2181ceb3187
Mark Silvis <marksilvis@pitt.edu>
2017-12-10 01:33:28 -0500

    Maps StorageDelegates to Long SUID (acquired from Network). Networks have 1-to-1 mapping to DB connection (StorageDelegate)

--- src/main/java/edu/pitt/cs/admt/cytoscape/annotations/db/StorageDelegateFactory.java
+++ src/main/java/edu/pitt/cs/admt/cytoscape/annotations/db/StorageDelegateFactory.java
@@ -2,7 +2,6 @@ package edu.pitt.cs.admt.cytoscape.annotations.db;
 
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import org.hsqldb.lib.Storage;
 
 /**
  * @author Nikos R. Katsipoulakis
@@ -11,26 +10,27 @@ public enum StorageDelegateFactory {
 
   INSTANCE;
 
-  private static final ConcurrentHashMap<String, StorageDelegate> index = new ConcurrentHashMap<>();
+  //  private static final ConcurrentHashMap<String, StorageDelegate> index = new ConcurrentHashMap<>();
+  private static final ConcurrentHashMap<Long, StorageDelegate> index = new ConcurrentHashMap<>(0);
 
-  public static StorageDelegate newDelegate() {
-    StorageDelegate delegate = new StorageDelegate();
-    index.putIfAbsent(delegate.getId(), delegate);
-    return delegate;
-  }
+//  public static StorageDelegate newDelegate() {
+//    StorageDelegate delegate = new StorageDelegate();
+//    index.putIfAbsent(delegate.getId(), delegate);
+//    return delegate;
+//  }
 
   public static StorageDelegate newDelegate(final Long networkSUID) {
     StorageDelegate delegate = new StorageDelegate(networkSUID);
-    index.putIfAbsent(networkSUID.toString(), delegate);
+    index.putIfAbsent(networkSUID, delegate);
     return delegate;
   }
 
   public static Optional<StorageDelegate> getDelegate(final Long networkSUID) {
-    return Optional.ofNullable(index.get(networkSUID.toString()));
+    return Optional.ofNullable(index.get(networkSUID));
   }
 
   public static boolean destroyDelegate(StorageDelegate delegate) {
-    return index.remove(delegate.getId(), delegate);
+    return index.remove(delegate.getNetwork(), delegate);
   }
 
   public static boolean destroyDelegateByNetwork(StorageDelegate delegate) {
