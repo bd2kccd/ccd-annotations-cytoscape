88ec08c6a1d5555e4ae9a6a1c60de40d62ee2ecf
Mark Silvis <marksilvis@pitt.edu>
2018-04-10 17:35:42 -0400

    moves CCD Annotation class to base package

--- src/main/java/edu/pitt/cs/admt/cytoscape/annotations/CCDAnnotation.java
+++ src/main/java/edu/pitt/cs/admt/cytoscape/annotations/CCDAnnotation.java
@@ -1,4 +1,4 @@
-package edu.pitt.cs.admt.cytoscape.annotations.view;
+package edu.pitt.cs.admt.cytoscape.annotations;
 
 /**
  * @author Mark Silvis (marksilvis@pitt.edu)
@@ -7,20 +7,4 @@ public class CCDAnnotation {
   public static final String CCD_NETWORK_ANNOTATIONS = "__CCD_Annotations";
   public static final String CCD_ANNOTATION_SET = "__CCD_Annotation_Set";
   public static final String ANNOTATION_SET = "__Annotations";
-
-/*
-    @Override
-    public boolean isSelected() {
-        return false;
-    }
-
-    @Override
-    public void setText(final String s) {
-    }
-
-    @Override
-    public String getText() {
-        return null;
-    }
-*/
 }
